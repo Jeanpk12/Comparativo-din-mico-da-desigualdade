287fcf1c5393f98f1f845ed8e706290c2691878f
jeanpk12 <jeanolivera123@gmail.com>
2023-12-01 02:40:07 -0300

    Atualização dos dados e refinamento do design

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
     let startTime = new Date();
 
     // Dados de Elon Musk
-    const elonMuskEarningsPerSecond = 10152; // U$ 10.152
+    const elonMuskEarningsPerSecond = 868; // U$ 868
     let elonMuskEarnings = 0;
 
     // Dados de fome no mundo
-    const secondsPerDeath = 3.5;
+    const secondsPerDeath = 4;
     let hungerDeaths = 0;
 
     // Atualizar os dados a cada segundo
@@ -19,11 +19,12 @@ document.addEventListener("DOMContentLoaded", function () {
         // Atualizar ganhos de Elon Musk
         elonMuskEarnings = Math.floor(elapsedTimeInSeconds * elonMuskEarningsPerSecond);
         let elonEarningsElement = document.getElementById("elon-earnings");
-        elonEarningsElement.innerHTML = `Ganho desde sua chegada: <span class="destaque">U$ ${elonMuskEarnings.toLocaleString()}</span>`;
+        elonEarningsElement.innerHTML = `Ganhou desde sua chegada: <span class="destaque">U$ ${elonMuskEarnings.toLocaleString()}</span>`;
 
         // Calcular mortes por fome no mundo
         hungerDeaths = Math.floor(elapsedTimeInSeconds / secondsPerDeath);
         let hungerDeathsElement = document.getElementById("hunger-deaths");
         hungerDeathsElement.innerHTML = `Pessoas mortas desde sua chegada: <span class="destaque">${hungerDeaths.toLocaleString()}</span>`;
     }, 1000);
+    
 });
